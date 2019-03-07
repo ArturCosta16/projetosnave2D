c4bc53431a15686a122846e024759711951df9b9
semflash <antoniiovictorx.p@gmail.com>
2019-03-07 14:50:50 -0300

    primeira versao do projeto

---
+++ PAD12D070319/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PAD12D070319
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+
+
+
+
+
+
+
+
+
+            Console.WriteLine("                           HELLO WORLD");
+        }
+    }
+}
